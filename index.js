bad7614a8056c5bba8227057d24b6b1138c3aa77
Abidur Niloy <niloyabidur@gmail.com>
2022-11-26 14:58:00 +0600

    server other route not working

--- index.js
+++ index.js
@@ -252,7 +252,9 @@ async function run() {
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
