ad1d4fe34de96110cc5837509772bce7288d53fa
Abidur Niloy <niloyabidur@gmail.com>
2022-11-28 13:13:30 +0600

    deploy

--- index.js
+++ index.js
@@ -249,12 +249,7 @@ async function run() {
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/home', (req, res) => {
-  res.send('Hello home')
-})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
